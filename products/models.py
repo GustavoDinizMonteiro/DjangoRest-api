a49bec95583ce9cfdbd1817ac4b6dad6a12448c2
Gustavo Diniz Monteiro <gustavo.monteiro@ccc.ufcg.edu.br>
2018-04-22 20:26:59 -0700

    Add model for 'products' resource and base class for test it.

--- products/models.py
+++ products/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class Product(models.Model):
+    name = models.CharField(max_length=50, blank=False, null=False)
+    price = models.FloatField(null=False)
+
+    class Meta:
+        verbose_name = u'Product'
+        verbose_name_plural = u'Products'
+
+
+    def __str__(self):
+        return "Name: %s, Price: %.2f" % (self.name, self.price)
---
+++ products/tests/tests_model.py
@@ -0,0 +1,11 @@
+from django.test import TestCase
+from model_mommy import mommy
+from products.models import Product
+
+class TestProduct(TestCase):
+    def setUp(self):
+        self.product = mommy.make(Product, name='Apple', price=1.99)
+
+
+    def test_product_creation(self):
+        self.assertTrue(isinstance(self.product, Product))

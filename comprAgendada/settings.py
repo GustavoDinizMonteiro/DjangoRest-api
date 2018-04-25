9163050d6aa609396bd0d3c17c9d4174fbdbadf3
Gustavo Diniz Monteiro <gustavo.monteiro@ccc.ufcg.edu.br>
2018-04-24 17:57:53 -0700

    Prepare system for deploy on Heroku

--- comprAgendada/settings.py
+++ comprAgendada/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 """
 
 import os
+import django_heroku
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -137,3 +139,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+load_dotenv(override=True)
+
+
+# Activate Django-Heroku.
+django_heroku.settings(locals())
